3a08987518da547f29cf925b2e8a2e663469515a
agrroc <a962758876@sina.com>
2018-09-07 16:49:28 +0800

    Initial Commit

---
+++ stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stack
+//
+//  Created by Agrroc-mac on 2018/9/7.
+//  Copyright © 2018年 Agrroc-mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
